8fbc1990b642e2177ba4c88242130cac99bab147
Joao Ferreira <joaobizuca10@gmail.com>
2026-03-12 22:28:44 -0300

    chore: including concurrency and build migrations check step

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -23,10 +23,8 @@ def get_url() -> str:
 
 
 def run_migrations_offline() -> None:
-    URL = get_url()
-
     context.configure(
-        url=URL,
+        url=get_url(),
         target_metadata=target_metadata,
         literal_binds=True,
         compare_type=True,
@@ -37,9 +35,8 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    URL = get_url()
     configuration = config.get_section(config.config_ini_section) or {}
-    configuration["sqlalchemy.url"] = URL
+    configuration["sqlalchemy.url"] = get_url()
 
     connectable = engine_from_config(
         configuration,
